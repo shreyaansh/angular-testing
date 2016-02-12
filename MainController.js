27b7b973409bebfb85e655b565c60ccfa13e4088
Shreyaansh Bassi <sbassi@purdue.edu>
2016-02-11 21:05:22 -0500

    Test run 1

--- MainController.js
+++ MainController.js
@@ -1,3 +1,4 @@
 app.controller("MainController", ['$scope', function($scope) {
 	$scope.title = 'Testing the application';
+	$scope.run = 'Run Successful';
 }]);
--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 var app = angular.module('myApp', []);
+app.controller("MainController", ['$scope', function($scope) {
+	$scope.title = 'Testing the application';
+	$scope.run = 'Run Successful';
+}]);
